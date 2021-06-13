ccff36e5991c2f1de82f311f6b5cb388bde60948
kt-std <kt.std2020@gmail.com>
2021-06-14 01:41:42 +0300

    Add search hints
    Enable search if media types are not selected

--- src/components/SearchButton/SearchButton.js
+++ src/components/SearchButton/SearchButton.js
@@ -2,9 +2,5 @@ import React from 'react';
 import styles from './style.css';
 
 export default function SearchButton({ selectedMediaTypes }) {
-  return (
-    <button className={styles.search__button} disabled={!selectedMediaTypes.length}>
-      search
-    </button>
-  );
+  return <button className={styles.search__button}>search</button>;
 }
